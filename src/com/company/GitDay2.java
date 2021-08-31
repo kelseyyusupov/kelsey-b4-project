2425795bae9da612f05d6b5b7a5e6a06526666e6
Kelsey Yusupov <kelsey.yusupov@gmail.com>
2021-08-31 08:40:32 -0500

    String Reverse

--- src/com/company/GitDay2.java
+++ src/com/company/GitDay2.java
@@ -3,5 +3,10 @@ package com.company;
 public class GitDay2 {
     public static void main(String[] args) {
         System.out.println("Good Morning! It's Git Day 2");
+        System.out.println("Hello World");
+    }
+
+    public static String revstr(String str) {
+        return new StringBuilder(str).reverse().toString();
     }
 }
